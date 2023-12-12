57a9b0db6359136c81658aa6bd37ccae11ff2073
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2023-12-12 09:06:31 +0200

    Add files via upload

---
+++ panic2_my.py
@@ -0,0 +1,13 @@
+phrase = 'Don`t panic!'
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+plist = plist[1:8]
+plist.remove('`')
+plist.insert(2, plist.pop(3))
+plist.insert(4, plist.pop())
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
